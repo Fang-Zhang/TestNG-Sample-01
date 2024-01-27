faf3d6694b804336cbb7f687609ac72a2fbd6cef
Fang Zhang <walter.zhangfang@gmail.com>
2024-01-28 00:08:19 +1300

    setup on win10

--- src/test/java/com/testng/tests/OrangeHRM.java
+++ src/test/java/com/testng/tests/OrangeHRM.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OrangeHRM {
@@ -17,7 +15,7 @@ public class OrangeHRM {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 	}
 	
 	@Test
