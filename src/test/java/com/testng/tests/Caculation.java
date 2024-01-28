e075c36db250a3b2ee9322e79168243980bb0e2d
walter <walter.zhangfang@gmail.com>
2024-01-28 19:02:00 +1300

    update the annotation testing.

---
+++ src/test/java/com/testng/tests/Caculation.java
@@ -0,0 +1,14 @@
+package com.testng.tests;
+
+public class Caculation {
+
+	public static void main(String[] args) {
+		System.out.println(new Caculation().add(1, 2));
+
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
