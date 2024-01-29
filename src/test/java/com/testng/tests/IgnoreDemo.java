2a5b88421e1e11c5c70b01b95558c724dd15a9dc
walter <walter.zhangfang@gmail.com>
2024-01-29 16:12:15 +1300

    Adding Ignore Annotation

--- src/test/java/com/testng/tests/IgnoreDemo.java
+++ src/test/java/com/testng/tests/IgnoreDemo.java
@@ -10,11 +10,13 @@ public class IgnoreDemo {
 		System.out.println("test1");
 	}
 
+	@Ignore
 	@Test(enabled = true)
 	public void test2() {
 		System.out.println("test2");
 	}
 
+	@Ignore
 	@Test(enabled = false)
 	public void test3() {
 		System.out.println("test3");
